b4ab33571889d70d9a0e884e668808114d1930af
Hamanist <hamanist@gmail.com>
2023-11-07 19:49:55 +0300

    implementation of functions

--- functions/functions.py
+++ functions/functions.py
@@ -2,15 +2,15 @@ import json
 from datetime import datetime
 
 
-def load_json():
-    with open('../file.json', 'r', encoding='utf-8') as f:
+def load_json(data):
+    with open(data, 'r', encoding='utf-8') as f:
         file_json = json.load(f)
         return file_json
 
 
-def executed_operations():
+def executed_operations(data):
     executed_list = []
-    for i in load_json():
+    for i in data:
         if i == {}:
             continue
         if i["state"] == 'EXECUTED':
@@ -18,27 +18,26 @@ def executed_operations():
     return executed_list
 
 
-def operations_by_date():
-    sort_date = sorted(executed_operations(), key=lambda date: date["date"], reverse=True)
-    return sort_date[:5]
+def operations_by_date(data, amount_of_data):
+    sort_date = sorted(data, key=lambda date: date["date"], reverse=True)
+    return sort_date[:amount_of_data]
 
 
-
-def conclusion():
-    for item in operations_by_date():
+def conclusion(data):
+    for item in data:
         date = datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
         description = item['description']
-
+        if "from" not in item:
+            item["from"] = 'Засекречено ХХХХХХХХХХХХХХХХ'
+        _from = item["from"]
+        card_name = _from.split(" ")
+        number = card_name.pop()
+        number = f'{number[:4]} {number[6:8]}** **** {number[-4:]}'
+        to = f"{item['to'][:5]}**{item['to'][-4:]}"
+        amount = item["operationAmount"]["amount"]
+        currency = item["operationAmount"]["currency"]["name"]
         print(date, description)
-        # print(data['from'], data['to'])
+        print(f'''{' '.join(card_name)} {number} -> {to}''')
+        print(amount, currency, "\n")
 
 
-conclusion()
-# Требования
-# Последние 5 выполненных (EXECUTED) операций выведены на экран.
-# Операции разделены пустой строкой.
-# Дата перевода представлена в формате ДД.ММ.ГГГГ (пример: 14.10.2018).
-# Сверху списка находятся самые последние операции (по дате).
-# Номер карты замаскирован и не отображается целиком в формате  XXXX XX** **** XXXX (видны первые 6 цифр и последние 4, разбито по блокам по 4 цифры, разделенных пробелом).
-# Номер счета замаскирован и не отображается целиком в формате  **XXXX
-# (видны только последние 4 цифры номера счета).
