ac96d47b8e61e9357945935e86f42935f1b85b2d
Hamanist <hamanist@gmail.com>
2023-11-09 00:13:27 +0300

    added tests

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,18 +1,25 @@
-from functions.functions import load_json, executed_operations
+from functions.functions import load_json, executed_operations, operations_by_date, conclusion
+from unittest.mock import patch, mock_open
+import json
 
 
 def test_load_json(list_fixture):
+    with patch("builtins.open", mock_open(read_data=json.dumps(list_fixture))) as mock_file:
+        assert load_json('')
 
 
-
-
-def test_executed_operations(list_fixture): ...
-
+def test_executed_operations(list_fixture):
+    assert executed_operations(list_fixture)
+    assert len(executed_operations([{}, {"state": "EXECUTED"}])) == 1
 
 
 def test_operations_by_date():
-    ...
+    list_data = [
+        {"date": "2019-06-30T15:11:53.136004"},
+        {"date": "2018-02-03T07:16:28.366141"}
+    ]
+    assert len(operations_by_date(list_data, 1)) == 1
 
 
-def test_conclusion():
-    ...
+def test_conclusion(list_fixture_2):
+    assert conclusion(list_fixture_2)
