fa5850a5239e0e6d2260a78fc0d1d50ceabdb8f0
Hamanist <hamanist@gmail.com>
2023-11-09 00:16:20 +0300

    add fix code

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ FILE = 'file.json'
 AMOUNT_OF_DATA = 5
 
 
-
 def main():
     data = load_json(FILE)
     data = executed_operations(data)
     data = operations_by_date(data, AMOUNT_OF_DATA)
     data = conclusion(data)
-    print(data)
+    for data_output in data:
+        print(data_output)
 
 
 if __name__ == '__main__':
