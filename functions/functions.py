bdbe4617ee7a85edbce1d638629e0a92751cbe28
Hamanist <hamanist@gmail.com>
2023-11-09 00:39:59 +0300

    add fix bag and add docstring

--- functions/functions.py
+++ functions/functions.py
@@ -3,12 +3,24 @@ from datetime import datetime
 
 
 def load_json(data):
+    """
+
+    :param data: принимает путь
+    :return: преобразует json в словарь
+    """
     with open(data, 'r', encoding='utf-8') as f:
         file_json = json.load(f)
         return file_json
 
 
 def executed_operations(data):
+    """
+
+    :param data: принимает словарь
+    :return: возвращает список словарей где
+    "state" == 'EXECUTED'
+
+    """
     executed_list = []
     for i in data:
         if i == {}:
@@ -19,11 +31,26 @@ def executed_operations(data):
 
 
 def operations_by_date(data, amount_of_data):
+    """
+
+    :param data: принимает список словарей
+    :param amount_of_data: выводит последние данные по индексу
+    :return: возвращает отсортированный список по дате
+    """
     sort_date = sorted(data, key=lambda date: date["date"], reverse=True)
     return sort_date[:amount_of_data]
 
 
 def conclusion(data):
+    """
+
+    :param data: отсортированный список по дате
+    :return: список формате:
+
+             <дата перевода> <описание перевода>
+             <откуда> -> <куда>
+             <сумма перевода> <валюта>
+    """
     data_output = []
     for item in data:
         date = datetime.strptime(item['date'], '%Y-%m-%dT%H:%M:%S.%f').strftime('%d.%m.%Y')
@@ -40,5 +67,3 @@ def conclusion(data):
         output = f"""{date} {description}\n{' '.join(card_name)} {number} -> {to}\n{amount} {currency}\n"""
         data_output.append(output)
     return data_output
-
-
